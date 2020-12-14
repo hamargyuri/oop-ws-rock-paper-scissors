31a76c8fbd106b93e06ada39dfea5fdb1bd4b4a2
hamargyuri <gyurinak@gmail.com>
2020-12-14 14:07:30 +0100

    Add lives field to RPS player class

--- src/players/RockPaperScissorsPlayer.java
+++ src/players/RockPaperScissorsPlayer.java
@@ -1,4 +1,8 @@
+package players;
+
 public abstract class RockPaperScissorsPlayer {
+    protected final Integer lives = 3;
+
     public abstract Integer getLives(); // starts from 3, lives decrease by one if a round is lost
     public abstract String showHand(); // should return "rock" / "paper" / "scissors"
     public abstract void decreaseLives(); // decrease lives by 1
