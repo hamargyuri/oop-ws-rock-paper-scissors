caff5433ba6428dfddbadf7a342cc45fd5df337d
morton2 <gab.marcell@gmail.com>
2020-12-14 14:25:27 +0100

    added player

---
+++ src/players/Marci.java
@@ -0,0 +1,18 @@
+package players;
+
+public class Marci extends RockPaperScissorsPlayer {
+    @Override
+    public Integer getLives() {
+        return null;
+    }
+
+    @Override
+    public String showHand() {
+        return null;
+    }
+
+    @Override
+    public void decreaseLives() {
+
+    }
+}
