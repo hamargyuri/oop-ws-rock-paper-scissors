b92226c2926bbdf35f9f6c72d81610ca9f5fcd6e
kisb01 <kisb01@gmail.com>
2020-12-14 14:42:28 +0100

    Add Player Janos

---
+++ src/players/JanosRPSPlayer.java
@@ -0,0 +1,19 @@
+package players;
+
+public class JanosRPSPlayer extends RockPaperScissorsPlayer {
+    @Override
+    public Integer getLives() {
+        return this.lives;
+    }
+
+    @Override
+    public String showHand() {
+        String[] possibleHands = new String[]{"rock", "paper", "scissors"};
+        return possibleHands[(int) (Math.random() * 3)];
+    }
+
+    @Override
+    public void decreaseLives() {
+        this.lives--;
+    }
+}
