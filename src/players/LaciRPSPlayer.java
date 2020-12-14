9fa6fb7830ef2a35cf729012b64b112540d675d0
Csabilaci <csabanyi.laszlo.zsolt@gmail.com>
2020-12-14 17:55:59 +0100

    LaciRPSPlayer created

--- src/players/LaciRPSPlayer.java
+++ src/players/LaciRPSPlayer.java
@@ -8,19 +8,12 @@ public class LaciRPSPlayer extends RockPaperScissorsPlayer {
 
         return this.lives;
     }
-
     @Override
     public String showHand() {
         String[] hands = new String[]{"rock", "paper", "scissor"};
-        String[] hands2 = new String[]{"paper", "scissor", "rock"};
         Random random = new Random(3);
-        Random rand2 = new Random(3);
-        if (random.equals("rock")) {
-            return hands2[rand2.nextInt()-1];
-        }
         return hands[random.nextInt() - 1];
     }
-
     @Override
     public void decreaseLives() {
         this.lives--;
