4884524f36f3eb86216597ff2256cec75a48a97d
hungenin <genin83@gmail.com>
2020-12-15 13:47:55 +0100

    Add new players

--- src/BalazsRockPaperScissors.java
+++ src/BalazsRockPaperScissors.java
@@ -5,17 +5,25 @@ public class BalazsRockPaperScissors{
         BalazsGame game = new BalazsGame();
 
         game.addPlayer(new AdamTeboldRPSPlayer());
+        game.addPlayer(new AndrasVasmaticsRPSPlayer());
+        game.addPlayer(new AntiRPSPlayer());
         game.addPlayer(new BalazsAI());
+        game.addPlayer(new BenceAI());
+        game.addPlayer(new ErikRPSPlayer());
         game.addPlayer(new GeriSzaszRPSPlayer());
+        //game.addPlayer(new hntsPlayer());
         game.addPlayer(new IZsolt());
         game.addPlayer(new JanosRPSPlayer());
         game.addPlayer(new KMarcellRPSPlayer());
+        game.addPlayer(new LaciRPSPlayer());
         game.addPlayer(new Marci());
         game.addPlayer(new NorbiRPSPlayer());
         game.addPlayer(new PJanosPlayer());
         game.addPlayer(new SzabiRPSPlayer());
         game.addPlayer(new Zemen());
         game.addPlayer(new Zoly());
+        game.addPlayer(new ZsofiSzakalRPSPlayer());
+        game.addPlayer(new ZsoltNemethRPSPlayer());
 
         game.autoPlay();
     }
