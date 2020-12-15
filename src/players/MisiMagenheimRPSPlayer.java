bc9e612e458bcdd6d5cd3d443aea7ae6e1ddc66d
Misi <misi.magenheim@gmail.com>
2020-12-15 13:34:51 +0100

    add myversion

---
+++ src/players/MisiMagenheimRPSPlayer.java
@@ -0,0 +1,22 @@
+package players;
+
+import java.util.Random;
+
+public class MisiMagenheimRPSPlayer extends RockPaperScissorsPlayer {
+    @Override
+    public Integer getLives() {
+        return this.lives;
+    }
+
+    @Override
+    public String showHand() {
+        String[]  possibleHands = new String[] {"rock", "paper", "scissors"};
+        Random random = new Random(3)
+        return possibleHands[random.nextInt() -1];
+    }
+
+    @Override
+    public void decreaseLives() {
+    this.lives --;
+    }
+}
